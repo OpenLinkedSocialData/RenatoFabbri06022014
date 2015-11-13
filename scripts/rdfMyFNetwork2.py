c9b59dafacf3a8a71ff8454c87752fa56e44634f
Renato Fabbri <renato.fabbri@gmail.com>
2015-11-12 23:32:06 -0200

    terminada primeira pubicacao da minha rede do fb

--- scripts/rdfMyFNetwork2.py
+++ scripts/rdfMyFNetwork2.py
@@ -2,10 +2,11 @@ import social as S, percolation as P
 import  importlib
 importlib.reload(S.fb)
 importlib.reload(P.rdf)
-fname="../data/RenatoFabbri06022014.gdf"
+fname="../data/fb/gdf/RenatoFabbri06022014.gdf"
 #fpath="/home/r/repos/linkedRFabbri/"
 fpath="./publishing/fb/"
-aname="rfabbri"
+aname=fname.split("/")[-1].split(".")[0]
+print(aname)
 S.fb.triplifyFriendshipNetwork(fname,fpath,aname)
 # after execution of these commands,
 # the fpath should have de files
